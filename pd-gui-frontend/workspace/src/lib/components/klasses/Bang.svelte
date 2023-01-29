db3c015335b20abe88065ec5ffc09eb930170778
Sebastian Stang <sebastian@chair.audio>
2023-01-29 15:23:39 +0100

    Fix Bang, make it work with different sizes

--- pd-gui-frontend/workspace/src/lib/components/klasses/Bang.svelte
+++ pd-gui-frontend/workspace/src/lib/components/klasses/Bang.svelte
@@ -4,10 +4,11 @@
 
   export let widget:PdWidget
   $: is_activated = widget.is_activated
+  $: box = widget.box
 </script>
 
 <NodeBase {widget} />
-<circle r={6} cx={7} cy={7} class:is_activated={$is_activated} />
+<circle r={$box.size.width / 2 - 2} cx={$box.size.width / 2 - 0.5} cy={$box.size.width / 2 - 0.5} class:is_activated={$is_activated} />
 
 <style lang="scss">
   circle {
