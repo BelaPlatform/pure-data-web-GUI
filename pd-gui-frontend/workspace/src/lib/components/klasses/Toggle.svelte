46ecc8d3a729ba83ec2bb3ca70b9b2176bf48c7a
Sebastian Stang <sebastian@chair.audio>
2023-02-03 10:37:00 +0100

    Implement fcolor for Toggle

--- pd-gui-frontend/workspace/src/lib/components/klasses/Toggle.svelte
+++ pd-gui-frontend/workspace/src/lib/components/klasses/Toggle.svelte
@@ -4,18 +4,21 @@
 
   export let widget:PdWidget
   $: is_activated = widget.is_activated
+  $: fcolor = widget.fcolor
 </script>
 
 <NodeBase {widget} />
-<line x1={2} x2={16} y1={2} y2={16} class:is_activated={$is_activated} />
-<line x1={2} x2={16} y1={16} y2={2} class:is_activated={$is_activated} />
+<g style:--fcolor={$fcolor}>
+  <line x1={2} x2={16} y1={2} y2={16} class:is_activated={$is_activated} />
+  <line x1={2} x2={16} y1={16} y2={2} class:is_activated={$is_activated} />
+</g>
 
 <style lang="scss">
   line {
     stroke: #fff;
     stroke-width: 0.5;
     &.is_activated {
-      stroke: #000;
+      stroke: var(--fcolor);
     }
   }
 </style>
