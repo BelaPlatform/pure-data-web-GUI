f1b642c3d1a81309a9dd8eb853b0b0756063de74
Sebastian Stang <sebastian@chair.audio>
2023-02-04 10:31:45 +0100

    Add dummy Find dialog

--- pd-gui-frontend/workspace/src/lib/components/pd/dialogs/FindDialog.svelte
+++ pd-gui-frontend/workspace/src/lib/components/pd/dialogs/FindDialog.svelte
@@ -1,1 +1,43 @@
-<h1>Find</h1>
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  import { pd } from '$lib/stores/pd'
+  import type { Fenster } from '$lib/stores/wm'
+  import * as G from '$lib/pd/geometry'
+
+  export let fenster: Fenster
+
+  let text: string
+  function on_send() {
+    console.log("searching " + text)
+    text = ""
+  }
+
+  onMount(() => {
+    fenster.set_title('Find')
+    fenster.set_size(new G.Size(288, 80))
+    fenster.set_is_resizable(false)
+  })
+</script>
+
+<div class="wrap">
+  Is there an API for this? 
+  <form on:submit|preventDefault={on_send}>
+    <input type="text" placeholder="Search in Pd for" bind:value={text} />
+  </form>
+</div>
+
+<style lang="scss">
+  .wrap {
+    padding: 12px;
+    font-family: 'Courier New', Courier, monospace;
+  }
+
+  form {
+    display: inline-block;
+  }
+
+  input {
+    size: 120px;
+  }
+</style>
--- pd-gui-frontend/workspace/src/lib/components/pd/dialogs/PreferencesDialog.svelte
+++ pd-gui-frontend/workspace/src/lib/components/pd/dialogs/PreferencesDialog.svelte
@@ -1,1 +1,27 @@
-<h1>Preferences</h1>
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  import { pd } from '$lib/stores/pd'
+  import type { Fenster } from '$lib/stores/wm'
+  import * as G from '$lib/pd/geometry'
+  
+  export let fenster: Fenster
+
+  onMount(() => {
+    fenster.set_title('Preferences')
+    fenster.set_size(new G.Size(480, 240))
+    fenster.set_is_resizable(false)
+  })
+</script>
+
+<div class="wrap">
+
+</div>
+
+<style lang="scss">
+  .wrap {
+    padding: 12px;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 12px;
+  }
+</style>
