de6ef67719a9a523191e1c0857750180cbb0a936
Sebastian Stang <sebastian@chair.audio>
2023-02-10 15:11:55 +0100

    Pass patch directory override to docker from run-dev.sh

--- pd-gui-frontend/run-dev.sh
+++ pd-gui-frontend/run-dev.sh
@@ -6,4 +6,5 @@ docker run -p 127.0.0.1:5173:5173 \
   --rm --interactive --tty \
   -v "$(pwd)/workspace":/workspace:z \
   -v "$(pwd)/../patches":/patches:z \
+  -e "OVERRIDE_PATCH_DIRECTORY=$(pwd)/../patches" \
   -h frontend --name pd-gui-frontend pd-gui-frontend
