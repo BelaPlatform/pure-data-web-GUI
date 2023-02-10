13eea368d0970acc4e0d7b474bb48a60e7ff4068
Sebastian Stang <sebastian@chair.audio>
2023-02-10 12:02:36 +0100

    Add run target to frontend's Dockerfile

---
+++ pd-gui-frontend/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker build --tag pd-gui-frontend-runner --target run .
+
+docker run -p 127.0.0.1:8080:3000 \
+  --rm --interactive --tty \
+  --name pd-gui-frontend-runner pd-gui-frontend-runner
