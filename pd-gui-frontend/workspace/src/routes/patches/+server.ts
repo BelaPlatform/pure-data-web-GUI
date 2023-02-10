7ae1849d013341734227b2b872bb977b1b0a938d
Sebastian Stang <sebastian@chair.audio>
2023-02-10 14:34:53 +0100

    Allow to override the patch directory with a environment variable passed to docker
    Required for testing with a locally installed pd

--- pd-gui-frontend/workspace/src/routes/patches/+server.ts
+++ pd-gui-frontend/workspace/src/routes/patches/+server.ts
@@ -4,11 +4,14 @@ import type { PatchFile } from '$lib/stores/patches'
 import { json } from '@sveltejs/kit'
 
 export async function GET() {
+  console.log(process.env)
+  const override_patch_directory = process.env.OVERRIDE_PATCH_DIRECTORY || "/patches"
+  console.log(override_patch_directory)
   let id = 1
   let patches: PatchFile[] = []
   const files = fs.readdirSync('/patches')
   fs.readdirSync('/patches').forEach(file => {
-    file = `/patches/${file}`
+    file = `${override_patch_directory}/${file}`
     patches.push({id, file})
     id++
   })
