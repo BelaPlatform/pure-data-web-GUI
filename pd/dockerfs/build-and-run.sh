7f0b30533d0d21918de37e295c0d394fdf6e1eed
Giulio Moro <giuliomoro@yahoo.it>
2023-02-20 12:25:48 -0500

    Only reconfigure if needed, making ./run.sh faster.
    As usual, this may not catch all corner cases. If you update your Pd code so that it needs to bereconfigured, make sure you force reconfig.

--- pd/dockerfs/build-and-run.sh
+++ pd/dockerfs/build-and-run.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+set -euo pipefail
+set -x
 
+PD_INSTALL_DIR=/opt/pd/
+PD_INSTALL_BINARY=$PD_INSTALL_DIR/bin/pd
+[ -f "Makefile.in" ] || {
 ./autogen.sh
-./configure --prefix /opt/pd --disable-portaudio
+}
+[ -f "Makefile" ] || {
+./configure --prefix $PD_INSTALL_DIR --disable-portaudio
+}
 make -j$(getconf _NPROCESSORS_ONLN) install
 
 set -m
 
-(sleep 4; /opt/pd/bin/pd -guiport 56026) &
+(sleep 4; $PD_INSTALL_BINARY -guiport 56026) &
 
 ssh pd-gui-shim-runner -L 127.0.0.1:56026:127.0.0.1:56026
 
