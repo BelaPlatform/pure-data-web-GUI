644c06ac760d363b8ea3e18a618d35fa484e2e68
Giulio Moro <giuliomoro@yahoo.it>
2023-02-23 20:09:46 -0500

    expose 8080 and 8081 on 0.0.0.0 so you can connect from the local network

--- pd-gui-frontend/run.sh
+++ pd-gui-frontend/run.sh
@@ -2,7 +2,7 @@
 
 docker build --tag pd-gui-frontend-runner --target run .
 
-docker run -p 127.0.0.1:8080:3000 \
+docker run -p 0.0.0.0:8080:3000 \
   --rm --interactive --tty \
   -v "$(pwd)/../patches":/patches:z \
   --network pd-gui-run \
--- pd-gui-shim/run.sh
+++ pd-gui-shim/run.sh
@@ -2,7 +2,7 @@
 
 docker build --tag pd-gui-shim-runner --target run .
 
-docker run -p 127.0.0.1:8081:8081 -p 127.0.0.1:56026:56026  -p 127.0.0.1:22:22 \
+docker run -p 0.0.0.0:8081:8081 -p 127.0.0.1:56026:56026  -p 127.0.0.1:22:22 \
   --rm --interactive --tty \
   --network pd-gui-run \
   --name pd-gui-shim-runner pd-gui-shim-runner
