f20a6f391f9a2e2670d074c1ca21db84c1d6444c
Giulio Moro <giuliomoro@yahoo.it>
2023-03-14 15:44:38 -0500

    docker run --init

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 docker build --tag pd-gui-runner --target run .
 
 docker run -p 0.0.0.0:8081:8081 -p 0.0.0.0:8080:3000 -p 0.0.0.0:8082:5173 \
+  --init \
   --rm --interactive --tty \
   -v "$(pwd)":/repo:z \
   -v "$(pwd)/patches":/patches:z \
