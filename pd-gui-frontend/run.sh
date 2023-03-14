ceef7107cd0c0d8aa0c31c8b6131d42f0ace470d
Giulio Moro <giuliomoro@yahoo.it>
2023-03-14 15:30:06 -0500

    docker run --init allows to fwd ctrl-C to running process

--- pd-gui-frontend/run.sh
+++ pd-gui-frontend/run.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 docker build --tag pd-gui-frontend-runner --target run .
 
 docker run -p 0.0.0.0:8080:3000 \
+  --init \
   --rm --interactive --tty \
   -v "$(pwd)/../patches":/patches:z \
   --network pd-gui-run \
