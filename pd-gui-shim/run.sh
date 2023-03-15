f267b2083a578c4be96738b6b0fb4ecaaa2510be
Giulio Moro <giuliomoro@yahoo.it>
2023-03-15 18:37:39 -0500

    do not mount workspace in prod

--- pd-gui-shim/run.sh
+++ pd-gui-shim/run.sh
@@ -5,7 +5,6 @@ docker build --tag pd-gui-shim-runner --target run .
 
 docker run -p 0.0.0.0:8081:8081 \
   --rm --interactive --tty \
-  -v "$(pwd)/workspace":/workspace:z \
   -v "$(pwd)/../patches":/patches:z \
   -v "$(pwd)/../pd/pure-data":/pure-data:z \
   --name pd-gui-shim-runner pd-gui-shim-runner
